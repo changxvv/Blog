9730525aab806df2cba06806bf4e9b6b25806e6d
changxv <48442578@qq.com>
2023-10-16 14:23:00 +0800

    delete todo section

--- main.py
+++ main.py
@@ -92,28 +92,8 @@ def get_repo(user: Github, repo: str):
 	return user.get_repo(repo)
 
 
-def parse_TODO(issue):
-	body = issue.body.splitlines()
-	todo_undone_pattern = re.compile(r'^(>?\s*- \[ \])')
-	todo_done_pattern = re.compile(r'^(>?\s*- \[x])')
-
-	todo_undone = [l for l in body if todo_undone_pattern.match(l)]
-	todo_done = [l for l in body if todo_done_pattern.match(l)]
-	# just add info all done
-	if not todo_undone:
-		return f"[{issue.title}]({issue.html_url}) all done.", []
-	return (
-		f"[{issue.title}]({issue.html_url}) —— {len(todo_undone)} jobs to do, {len(todo_done)} jobs done.",
-		todo_done + todo_undone,
-	)
-
-
 def get_top_issues(repo):
 	return repo.get_issues(labels=TOP_ISSUES_LABELS)
-
-
-def get_todo_issues(repo):
-	return repo.get_issues(labels=TODO_ISSUES_LABELS)
 
 
 def get_about_me_issues(repo):
@@ -133,22 +113,6 @@ def add_issue_info(issue, md):
 	update_time = format_time(issue.updated_at)
 	# ![created on](https://img.shields.io/badge/created%20on-{create_time}-blue) 
 	md.write(f"- [{issue.title}]({issue.html_url}) ![updated on](https://img.shields.io/badge/updated-{update_time}-green)\n")
-
-
-def add_md_todo(repo, md, me):
-	todo_issues = list(get_todo_issues(repo))
-	if not TODO_ISSUES_LABELS or not todo_issues:
-		return
-	with open(md, "a+", encoding="utf-8") as md:
-		md.write("## TODO\n")
-		for issue in todo_issues:
-			if is_me(issue, me):
-				todo_title, todo_list = parse_TODO(issue)
-				md.write("<details>\n<summary>TODO list from " + todo_title + "</summary>\n\n")
-				for t in todo_list:
-					md.write(t + "\n")
-				# new line
-				md.write("\n</details>\n")
 
 
 def add_md_top(repo, md, me):
@@ -218,7 +182,7 @@ def add_md_about_me(repo, md, me):
 def add_md_header(md, repo_name):
 	with open(md, "w", encoding="utf-8") as md:
 		md.write(MD_HEAD.format(repo_name=repo_name))
-		md.write("\n\n")
+		md.write("\n")
 
 
 def add_md_label(repo, md, me):
@@ -252,13 +216,12 @@ def add_md_label(repo, md, me):
 					continue
 				if is_me(issue, me):
 					if i == ANCHOR_NUMBER:
-						md.write("<details><summary>显示更多</summary>\n")
-						md.write("\n")
+						md.write("<details><summary>显示更多</summary>\n\n")
 					add_issue_info(issue, md)
 					i += 1
 			if i > ANCHOR_NUMBER:
 				md.write("</details>\n")
-				md.write("\n\n")
+			md.write("\n")
 
 
 def get_to_generate_issues(repo, dir_name, issue_number=None):
@@ -309,7 +272,7 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
 	repo = get_repo(user, repo_name)
 	# add to readme one by one, change order here
 	add_md_header("README.md", repo_name)
-	for func in [add_md_about_me, add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
+	for func in [add_md_about_me, add_md_firends, add_md_top, add_md_recent, add_md_label]:
 		func(repo, "README.md", me)
 
 	generate_rss_feed(repo, "feed.xml", me)
